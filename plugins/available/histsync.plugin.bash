a70a0e793e36d3be8c6d00b7776fa394a8b57327
Yushin Washio <yuwash@yandex.com>
2017-08-09 13:13:48 +0500

    histsync.plugin can make backups

--- plugins/available/histsync.plugin.bash
+++ plugins/available/histsync.plugin.bash
@@ -1,6 +1,15 @@
 cite 'about-plugin'
 about-plugin 'Bash history save and synchronization'
 
+HFBASE="`basename $HISTFILE`"
+HFBASE="${HFBASE#.}"
+
+
+function _file_iso_date () {
+	ls -l --time-style=long-iso "$1" | grep -m 1 -o '\<[[:digit:]]\{4\}\(-[[:digit:]]\{2\}\)\{2\}\>'
+}
+
+
 # quick way to synchronize history with other bash sessions
 # from https://superuser.com/a/602405
 function histsync () {
@@ -9,4 +18,18 @@ function histsync () {
 	group 'histsync'
 
 	history -a && history -c && history -r
+	for target in "$@"
+	do
+		dest="$target/${HFBASE}-latest"
+		if [[ -e "$dest" ]]
+		then
+			if diff "$HISTFILE" "$dest"
+			then
+				echo skipping up-to-date destination $dest
+				continue
+			fi
+			suffix="-`_file_iso_date "$dest"`"
+			mv -i "$dest" "${dest%-latest}$suffix"
+		fi && cp -ai "$HISTFILE" "$dest"
+	done
 }
