fd79ff61935d3d706b2720282200da4302c334ed
Yushin Washio <yuwash@yandex.com>
2017-08-11 12:39:16 +0200

    davencmount.plugin add routine to initialize config file using /etc/fstab

--- plugins/available/davencmount.plugin.bash
+++ plugins/available/davencmount.plugin.bash
@@ -1,6 +1,9 @@
 cite 'about-plugin'
 about-plugin 'mount davfs2 and encfs as configured in ~/.davencmount'
 
+DAVENCMOUNT_FILE="$HOME/.davencmount"
+
+
 function _mounted () {
 	if ! [[ -z $2 ]]
 	then echo _mounted ignoring surplus arguments $2 ..
@@ -9,11 +12,82 @@ function _mounted () {
 }
 
 
+function _choose () {
+	choices="$1"
+	[[ -z "$choices" ]] && return 1  # choices can't be empty
+	defchoice="`grep -o '[[:upper:]]' <<< "$choices"`"
+	((1 >= ${#defchoice})) || return 1  # only one default choice supported
+	hint="`sed -e 's/\w/&\//g' -e 's/\/$//' <<< $choices`"
+	shift
+	echo -n "$@ ($hint) " > /dev/tty
+	while true
+	do
+		read -n 1 ans
+		if [[ -z "$ans" ]] && ! [[ -z "$defchoice" ]]
+		then
+			echo $defchoice
+			return
+		fi
+		echo > /dev/tty  # finish last prompt line
+		while read -n 1 c
+		do if grep -qi $c <<< "$ans"
+		then
+			echo $c
+			return
+		fi
+		done <<< "$choices"
+		echo "Unrecognized option '$ans'" > /dev/tty
+		echo -n "Please choose one of ($hint) " > /dev/tty
+	done
+}
+
+
+function _confirm () {
+	choice="`_choose Yn $@`"
+	[[ "$choice" = Y ]]
+}
+
+
 function davencmount () {
 	about 'mount as configured in ~/.davencmount'
 	example '$ davencmount mytarget'
 	group 'davencmount'
 
+	[[ -e "$DAVENCMOUNT_FILE" ]] || if _confirm "davencmount configuration file at $DAVENCMOUNT_FILE doesn't exist. Create?"
+	then 
+		touch "$DAVENCMOUNT_FILE"
+		assume_yes=''
+		while read line
+		do
+			target="`read -a tmp <<<"$line" && echo "${tmp[1]}"`"  # second entry in fstab is mount target
+			if [[ "$assume_yes" = Y ]]
+			then ans=Y
+			else
+				ans="`_choose Yanq "add target $target? (yes/all/no/quit)" < /dev/tty`"
+				case "$ans" in
+					(a) ans=Y; assume_yes=Y;;
+					(q) break;;
+				esac
+			fi
+			if [[ "$ans" = Y ]]
+			then
+				echo -n "Give a unique shorthand for $target: "
+				while true
+				do
+					read shorthand < /dev/tty
+					if grep -q '^[[:alnum:]]\+$' <<< "$shorthand"
+					then
+						tee -a "$DAVENCMOUNT_FILE" <<< "$shorthand $target"
+						break
+					else echo -n 'Please only use alphanumeric characters: '
+					fi
+				done
+			fi
+		done < <(grep '^[^# ]\+\s\+/\S\+\s*davfs' < /etc/fstab)
+	else
+		echo davencmount doesn\'t work without configuration file\; aborting.
+		return 2
+	fi
 	unmount=''
 	for target in "$@"
 	do if [[ $target = -u ]]
@@ -21,7 +95,7 @@ function davencmount () {
 		unmount='yes'
 	else if [[ -z $unmount ]]
 	then 
-		grep "^$target\>" $HOME/.davencmount | while read entry
+		grep "^$target\>" "$DAVENCMOUNT_FILE" | while read entry
 		do etmp="`envsubst <<<"$entry"`" && read -a earr <<<"$etmp"
 			if [[ -z "${earr[1]}" ]]
 			then echo error at $entry; return 1
@@ -36,7 +110,7 @@ function davencmount () {
 			}
 		done
 	else
-		grep "^$target\>" $HOME/.davencmount | tac - | while read entry
+		grep "^$target\>" "$DAVENCMOUNT_FILE" | tac - | while read entry
 		do etmp="`envsubst <<<"$entry"`" && read -a earr <<<"$etmp"
 			if [[ -z "${earr[1]}" ]]
 			then echo error at $entry; return 1
